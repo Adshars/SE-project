f2a6a168ddbc739d9ca0e10b88f8065b09054d98
Adshars <165464514+Adshars@users.noreply.github.com>
2025-04-29 20:39:25 +0200

    Update sanctions_checker.py

--- kyc-service/app/sanctions_checker.py
+++ kyc-service/app/sanctions_checker.py
@@ -1,23 +1,33 @@
 import json
 import os
 
-# Załaduj dane sankcyjne przy starcie serwisu
-DATA_FILE = "/app/data/entities.json"  # Ścieżka do danych sankcyjnych
-SANCTIONS_DATA = []
+# Ścieżka do danych
+DATA_FILE = os.path.join('Data', 'Fixed', 'targets_fixed.json')
 
-if os.path.exists(DATA_FILE):
-    with open(DATA_FILE, "r", encoding="utf-8") as f:
-        SANCTIONS_DATA = json.load(f)
+#Załadowanie danych z pliku JSON
+with open(DATA_FILE, 'r', encoding='utf-8') as f:
+    data = json.load(f)
 
-def check_person(name, dob=None):
-    for entity in SANCTIONS_DATA:
-        entity_name = entity.get("name", "").lower()
-        if name.lower() in entity_name:
-            if dob:
-                # Jeśli podano datę urodzenia, sprawdzaj także ją
-                entity_dob = entity.get("birthDate")
-                if entity_dob and dob in entity_dob:
-                    return entity
+def search_person(first_name: str, last_name: str, birth_date: str = None):
+    """
+    Szukaj osoby w danych sankcyjnych.
+    """
+    resoults = []
+
+    for entry in DATA:
+        props = entry.get('properties', {})
+        entry_first_names = props.get('firstName', [])
+        entry_last_names = props.get('lastName', [])
+        entry_birth_dates = props.get('birthDate', None)
+
+        # Sprawdź, czy imię i nazwisko pasują
+        if (first_name in entry_first_names or first_name.lower in [n.lower() for n in entry_first_names]) and \
+            (last_name in entry_last_names or last_name.lower in [n.lower() for n in entry_last_names]):
+            # Sprawdź datę urodzenia, jeśli podano
+            if birth_date:
+                if entry_birth_dates and birth_date in entry_birth_dates:
+                    resoults.append(entry)
             else:
-                return entity
-    return None
+                resoults.append(entry)
+
+        return resoults
