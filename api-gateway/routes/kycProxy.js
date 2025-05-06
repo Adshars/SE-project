f1dfd6123732266f35880445e4e9a1b3b47e1a0f
Adshars <165464514+Adshars@users.noreply.github.com>
2025-05-06 21:18:34 +0200

    Update kycProxy.js

--- api-gateway/routes/kycProxy.js
+++ api-gateway/routes/kycProxy.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { createProxyMiddleware } from 'http-proxy-middleware';
-import { verifyToken } from '../middleware/verifyToken';
+import { verifyToken } from '../middleware/verifyToken.js';
 
 const router = express.Router();
 
